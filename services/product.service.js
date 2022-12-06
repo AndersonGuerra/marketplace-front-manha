6e0bd6b133020edf2a7131c8742160470839452f
Anderson Guerra <and.guerra@outlook.com>
2022-12-06 11:28:31 -0300

    Aual 06/12/22 att

--- services/product.service.js
+++ services/product.service.js
@@ -3,7 +3,16 @@ import api from "../api";
 export default {
   async getProducts() {
     const result = await api.get("/products");
-    console.log(result, "ahoy")
+    return result.data;
+  },
+  async getProduct(id) {
+    const result = await api.get(`/products/${id}`);
+    return result.data;
+  },
+  async createProduct(data) {
+    data.createdAt = new Date().toLocaleString();
+    data.image = "https://i.dummyjson.com/data/products/4/thumbnail.jpg";
+    const result = await api.post("/products", data);
     return result.data;
   },
 };
